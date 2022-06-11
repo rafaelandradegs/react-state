a7f5bfd1703c3967c2d3aad4b5a10dfb4d3dd421
Rafael A Goncalves <rafaelandradegs@gmail.com>
2022-06-11 02:36:49 -0300

    Detalhes sobre o funcionamento de useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import React, { useState } from 'react'
 
 export function App() {
-  const [valor, setValor] = useState(0)
+  // const [valor, setValor] = useState(10)
 
-  function incrementar() {
-    setValor(valor + 1)
-  }
+  // function decremento() {
+  //   setValor(valor - 1)
+  // }
+  // function incremento() {
+  //   setValor(valor + 1)
+  // }
+
+  useState()
+  useState()
+  useState()
+  useState()
 
   return (
     <>
       <h1>React Hooks</h1>
-      <h3>Valor = {valor}</h3>
-      <button onClick={incrementar}>Incrementar</button>
+      {/* <p>Valor: {valor}</p>
+      <button onClick={decremento}>Diminuir valor</button>
+      <button onClick={incremento}>Diminuir valor</button> */}
     </>
   )
 }
