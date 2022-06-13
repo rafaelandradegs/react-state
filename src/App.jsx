b800b3ce0c9b7a16449596ceab563fcdb11dc366
Rafael A Goncalves <rafaelandradegs@gmail.com>
2022-06-13 15:37:53 -0300

    Detalhes sobre o useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,46 @@
 import React, { useState, useEffect } from 'react'
 
 export function App() {
+  const clientes = ['Joao', 'Ana', 'Carlos']
+  const produtos = ['Pao', 'Manteiga', 'Leite']
+  const vendas = ['1 x Pao (Joao)', '2 x Manteiga (Ana)', '3 x Leite (Carlos)']
+
+  const [info, setInfo] = useState('clientes')
   const [valor, setValor] = useState(100)
-  const [valor2, setValor2] = useState(1000)
-
-  function alterar() {
-    setValor(valor + 10)
-  }
-
-  function alterar2() {
-    setValor2(valor2 + 10)
-  }
 
   useEffect(() => {
-    console.log('alterar')
-  }, [valor2])
+    // console.clear()
+    switch (info) {
+      case 'clientes':
+        console.table(clientes)
+        break
+
+      case 'produtos':
+        console.table(produtos)
+        break
+
+      case 'vendas':
+        console.table(vendas)
+        break
+    }
+  }, [info])
+
+  useEffect(() => {
+    console.log('Teste')
+  }, [valor])
 
   return (
     <>
       <h1>React Hooks - useEffect</h1>
-      <h3>Valor: {valor}</h3>
-      <button onClick={alterar}>Alterar</button>
-      <button onClick={alterar2}>Alterar2</button>
+      <hr />
+      <button onClick={() => setInfo('clientes')}>Cliente</button>
+      <button onClick={() => setInfo('produtos')}>Produtos</button>
+      <button onClick={() => setInfo('vendas')}>Vedas</button>
+      <h3>{info}</h3>
+
+      <h3>{valor}</h3>
+
+      <button onClick={() => setValor(valor + 10)}>Almentar</button>
     </>
   )
 }
