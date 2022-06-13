1827ebcce9c44b0ecb6e06e7462c09ecc175a52a
Rafael A Goncalves <rafaelandradegs@gmail.com>
2022-06-13 17:55:03 -0300

    Atencao do uso do userRef

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,29 @@ import React, { useRef, useState, useEffect } from 'react'
 
 export function App() {
   const [texto, setTexto] = useState('')
-  const inputRef = useRef()
-  const input2Ref = useRef()
+  const anterior = useRef('')
 
-  function focus1() {
-    inputRef.current.focus()
-  }
-  function alterar_texto2() {
-    input2Ref.current.value = 'Ola '
-    input2Ref.current.focus()
-  }
+  useEffect(() => {
+    anterior.current = texto
+  }, [texto])
+
+  function executar() {}
 
   return (
     <>
       <h1>React Hooks - useRef</h1>
       <hr />
       <input
-        ref={inputRef}
         type="text"
-        value={texto}
         onChange={e => {
           setTexto(e.target.value)
         }}
       />
-      <br />
-      <input type="text" ref={input2Ref} />
       <hr />
-      <button onClick={focus1}>Focus 1</button>
-      <button onClick={alterar_texto2}>Alterar texto 2</button>
+      <button onClick={executar}>Executar </button>
+      <p>
+        Texto: {texto} (anteriormente) {anterior.current}
+      </p>
     </>
   )
 }
