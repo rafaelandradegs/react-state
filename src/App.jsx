a381a1e5f060aff3ebe89bdef5e92a44ec96eda0
Rafael A Goncalves <rafaelandradegs@gmail.com>
2022-06-13 17:40:15 -0300

    Utilizacao mais comum do useRef

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,34 @@ import React, { useRef, useState, useEffect } from 'react'
 
 export function App() {
   const [texto, setTexto] = useState('')
-  const total = useRef(0)
+  const inputRef = useRef()
+  const input2Ref = useRef()
 
-  useEffect(() => {
-    total.current++
-  })
+  function focus1() {
+    inputRef.current.focus()
+  }
+  function alterar_texto2() {
+    input2Ref.current.value = 'Ola '
+    input2Ref.current.focus()
+  }
 
   return (
     <>
       <h1>React Hooks - useRef</h1>
       <hr />
       <input
+        ref={inputRef}
         type="text"
         value={texto}
         onChange={e => {
           setTexto(e.target.value)
         }}
       />
+      <br />
+      <input type="text" ref={input2Ref} />
       <hr />
-      <p>O texto digitado e: {texto}</p>
-      <hr />
-      <p>Total: {total.current}</p>
+      <button onClick={focus1}>Focus 1</button>
+      <button onClick={alterar_texto2}>Alterar texto 2</button>
     </>
   )
 }
