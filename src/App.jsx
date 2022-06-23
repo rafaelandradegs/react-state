a659d62e410bf9e46e385523f1b490b569bea577
Rafael A Goncalves <rafaelandradegs@gmail.com>
2022-06-23 15:35:36 -0300

    useId

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,24 @@
-import React, { useState, useCallback } from 'react'
-import { Lista } from './Lista'
+import React, { useId } from 'react'
 
 export function App() {
-  const [numero, setNumero] = useState(() => {
-    return 100
-  })
-  const [temaEscuro, setTemaEscuro] = useState(() => {
-    return false
-  })
-
-  const tema = {
-    background: temaEscuro ? 'black' : 'white',
-    color: temaEscuro ? 'white' : 'black',
-    height: '100vh'
-  }
-
-  const resultados = useCallback(() => {
-    return [numero * 2, numero * 3, numero * 4]
-  }, [numero])
+  const id1 = useId()
+  const id2 = useId()
 
   return (
-    <div style={tema}>
-      <h1>REACT - useCallback</h1>
+    <>
+      <h1>React - useId</h1>
       <hr />
-      <button
-        onClick={() => {
-          setTemaEscuro(!temaEscuro)
-        }}
-      >
-        Alterar Tema
-      </button>
-      <p>Numero: {numero}</p>
-      <button
-        onClick={() => {
-          setNumero(numero + 1)
-        }}
-      >
-        Incrementar
-      </button>
-      <hr />
-      <Lista resultados={resultados} />
-    </div>
+      <form action="#" method="post">
+        <div>
+          <input type="checkbox" name="checkbox1" id={id1} />
+          <label htmlFor={id1}>CheckBox 1</label>
+        </div>
+        <hr />
+        <div>
+          <input type="checkbox" name="checkbox2" id={id2} />
+          <label htmlFor={id2}>CheckBox 2</label>
+        </div>
+      </form>
+    </>
   )
 }
